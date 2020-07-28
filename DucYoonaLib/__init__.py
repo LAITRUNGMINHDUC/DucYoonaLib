90ceff7f0d64a753a2e213c5abdf25448a8a3a6c
Duc LTM <laitrungminhduc@hotmail.com>
2020-07-28 14:27:02 +0700

    Public access for some functions

--- DucYoonaLib/__init__.py
+++ DucYoonaLib/__init__.py
@@ -1,3 +1,3 @@
-from .multicores import multiprocessing_function_dataframe, multiprocessing_function_general, multiprocessing_read_xlsx_files
+from .multicores import multiprocessing_function_dataframe, multiprocessing_function_general, multiprocessing_read_xlsx_files, singlecore_processing_function_dataframe, singlecore_processing_function_general
 from .notification import alert_to_microsoft_teams, alert_to_slack
-from .utils import safe_division
+from .utils import safe_division, standardize_string
