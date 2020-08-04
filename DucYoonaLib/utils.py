ff3b27b08b883e5493a6af40b527eec3678936a3
Duc LTM <laitrungminhduc@hotmail.com>
2020-08-04 16:08:00 +0700

    Add Export Excel function with Standard Format

--- DucYoonaLib/utils.py
+++ DucYoonaLib/utils.py
@@ -7,3 +7,8 @@ def standardize_string(my_str):
             my_str = my_str.replace(char, '---')
     my_str = my_str.strip().upper()
     return my_str
+
+def export_excel(df, filepath, sheet_name='DATA'):
+	import pandas as pd
+	writer = pd.ExcelWriter(filepath, date_format='YYYY-MM-DD', datetime_format='YYYY-MM-DD')
+	df.to_excel(writer, sheet_name= sheet_name, index = False)
